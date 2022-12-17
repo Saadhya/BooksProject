f111ba8b95db8b491e5ed9b1d464ba8edba7763b
SAADHYA <saadhya_soni@mohawkind.com>
2022-12-17 15:31:39 +0530

    added boooks from google apps

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const API_KEY =
+  "sarajevo::stepzen.net+1000::56b3cf74abc045007ced50ac6a25dca4e867a99293cf41be35772e62b4afcc97";
+
+const client = new ApolloClient({
+  uri: "https://sarajevo.stepzen.net/api/killjoy-vulture/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,7 +26,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,108 @@
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, StyleSheet, FlatList } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+import { gql } from "@apollo/client/core";
+import { useQuery } from "@apollo/client";
+import BookItem from "../components/BookItem";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`;
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: "React Native" },
+  });
+
+  // console.log(JSON.stringify(data, null, 2));
+  // console.log(data)
+  // console.log(loading)
+  // console.log(error);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      {loading && <ActivityIndicator />}
+      {error && (
+        <View style={styles.container}>
+          <Text style={styles.title}>Error fetching books</Text>
+          <Text>{error.message}</Text>
+        </View>
+      )}
+      <FlatList
+        data={data?.googleBooksSearch?.items || []}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <BookItem
+            book={{
+              image: item.volumeInfo.imageLinks?.thumbnail,
+              title: item.volumeInfo.title,
+              authors: item.volumeInfo.authors,
+              isbn: item.volumeInfo.industryIdentifiers[0].identifier,
+            }}
+          />
+        )}
+      />
     </View>
   );
 }
 
+// export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>HELLO WORLD- first tab</Text>
+//       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+//       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+//     </View>
+//   );
+// }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: "center",
+    // justifyContent: "center",
+    color: "white",
+    padding:10,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
